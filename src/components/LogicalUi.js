8b83f8ba7542a6616e4ae292c4ac199135f75e81
mengmingxiu <mengmingxiu@100tal.com>
2019-07-01 10:14:12 +0800

    数据逻辑与UId的结合（1）

---
+++ src/components/LogicalUi.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class LogicalUi extends Component {
+  render() {
+    let style = {
+      width: 100,
+      height: 100,
+      background: this.props.bgColor || '#f00',
+      borderRadius: '50%'
+    };
+    return <div style={style}>LogicalUi</div>;
+  }
+}
+
+export default LogicalUi;
